08d5e6fe05ca5450f7a992ccf753e594857d3c28
Kennedy Murphy <kennedy.murphy@fiddlehead.io>
2019-01-04 08:43:02 -0400

    Renames make_dataset to make_database

--- src/data/make_database.py
+++ src/data/make_database.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def main():
+    """ Reads in raw data pulled via make data and generates a Postgresql
+        database.
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
