fe89404bde3225543dbc3455a5b08587412b5fc8
Kennedy Murphy <kennedy.murphy@fiddlehead.io>
2019-04-08 08:44:53 -0300

    Created data parsing to CSV in make_database to allow fast data exploration.

--- src/data/make_database.py
+++ src/data/make_database.py
@@ -1,7 +1,48 @@
 # -*- coding: utf-8 -*-
 import logging
+import pandas as pd
+import numpy as np
+import src.features.build_features as feats
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+
+
+def train_validation_split(valprop = 0.2):
+    """ Generates features for train, test set and
+        splits the training set into training and validation
+        sets.
+
+        :param valprop:     Proportion of training observations
+                                for validation
+    """
+    logger = logging.getLogger(__name__)
+    logger.info(f"Creating train/validation set split of {1-valprop}/{valprop}")
+
+    train_data, test_data = feats.build_transaction_data()
+    target_data = pd.read_csv('data/raw/train.csv', usecols=['card_id', 'target'])
+    target_data.set_index('card_id', inplace=True)
+
+    train_ids = train_data.index.values
+    validation_ids = np.random.choice(train_ids, int(len(train_data) * valprop))
+
+    logger.info(f"Selecting {len(validation_ids)} of {len(train_ids)} training " +
+        "observations for validation set."
+    )
+
+    logger.info("Saving validation set data...")
+    train_data.loc[validation_ids].to_csv(
+        "data/processed/validation_features.csv")
+    target_data.loc[validation_ids].to_csv(
+        "data/processed/validation_target.csv")
+
+    logger.info("Saving training set data...")
+    train_data.loc[~train_data.index.isin(validation_ids)].to_csv(
+        "data/processed/train_features.csv")
+    target_data.loc[~train_data.index.isin(validation_ids)].to_csv(
+        "data/processed/train_target.csv")
+
+    logger.info("Saving testing features")
+    test_data.to_csv("data/processed/test_features.csv")
 
 
 def main():
@@ -10,6 +51,7 @@ def main():
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    train_validation_split(0.2)
 
 
 if __name__ == '__main__':
