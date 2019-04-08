4d0582ad51af6f28ff1f091a22d27a0f07375a70
Kennedy Murphy <kennedy.murphy@fiddlehead.io>
2019-04-08 19:26:24 -0300

    Created feature selection notebook. Refactor train_perceptron to read saved CSV files and use validation set.

--- src/models/train_perceptron.py
+++ src/models/train_perceptron.py
@@ -9,44 +9,46 @@ from src.features import build_features
 from keras.models import Sequential
 from keras.layers import Dense
 
+# Set up data dir
+data_dir = "data/processed/"
+
 # Define arguments to be passed to model
 epochs=50
 batch_size=64
 learning_rate=0.01
 num_outputs=1
-num_examples=60000
-
 
 log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.INFO, format=log_fmt)
 logger = logging.getLogger(__name__)
 
+########################### Training cycle ###########################
 # Read in the data
-logger.info("Loading data")
-train_data, test_data = build_features.build_transaction_data()
-target_data = pd.read_csv('data/raw/train.csv', usecols=['target'], dtype=np.float32)
+logger.info("Loading training data")
+train_feats = pd.read_csv(f"{data_dir}train_features.csv")
+train_target = pd.read_csv(f"{data_dir}train_target.csv")
 
-# Cast int64 to float64
-for col in train_data.select_dtypes(['int', 'float']).columns:
-    train_data[col] = train_data[col].astype(np.float32)
+train_feats.set_index("card_id", inplace=True)
+train_target.set_index("card_id", inplace=True)
+
+# # Cast int64 to float64
+# for col in train_data.select_dtypes(['int', 'float']).columns:
+#     train_data[col] = train_data[col].astype(np.float32)
     
-    assert col in test_data.columns
-    test_data[col] = test_data[col].astype(np.float32)
+#     assert col in test_data.columns
+#     test_data[col] = test_data[col].astype(np.float32)
 
-target_data['target'] = target_data.target.astype(np.float32)
+# target_data['target'] = target_data.target.astype(np.float32)
 
 logger.info("Creating input matrices")
-X_train = train_data[[c for c in train_data.columns]].values
-y_train = target_data.target.values
-
-X_test = test_data[[c for c in test_data.columns]].values 
-test_ids = test_data.index.values
+X_train = train_feats.values
+y_train = train_target.values.reshape(-1)
 
 # Setup the number of inputs
 num_inputs=X_train.shape[1]
 num_hidden=num_inputs * 2
 
-del train_data, test_data, target_data
+del train_feats, train_target
 gc.collect()
 
 logger.info("Defining Perceptron")
@@ -59,11 +61,26 @@ model.compile(optimizer='rmsprop', loss='mse')
 logger.info(f"Starting training process with epochs:{epochs} & batch size:{batch_size}")
 model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
 
+########################### Validation Cycle ###########################
+# logger.info("Loading validation data")
+# val_feats = pd.read_csv(f"{data_dir}validation_features.csv")
+# val_target = pd.read_csv(f"{data_dir}validation_target.csv")
+
+# val_feats.set_index("card_id", inplace=True)
+# val_target.set_index("card_id", inplace=True)
+
+# del val_feats, val_target
+########################### Prediction Cycle ###########################
+test_feats = pd.read_csv(f"{data_dir}test_features.csv")
+test_feats.set_index("card_id", inplace=True)
+
+X_test = test_feats.values
+
 logger.info(f"Creating predictions with batch size:{batch_size}")
 y_pred = model.predict(X_test, batch_size=batch_size, verbose=1)
 
 logger.info(f"Saving predictions")
-df_test = pd.DataFrame({'card_id': test_ids, 'target': y_pred.reshape(-1, )})
+df_test = pd.DataFrame({'card_id': test_feats.index.values, 'target': y_pred.reshape(-1, )})
 
 if df_test.target.isnull().any():
     null_count = df_test.target.isnull().sum()
