b16be9672c63d9aae72e87dad6816cba91dae5b6
Kennedy Murphy <kennedy.murphy@fiddlehead.io>
2019-04-25 17:39:21 -0300

    Added correct normal outlers function to outlier_correction

--- src/features/outlier_correction.py
+++ src/features/outlier_correction.py
@@ -19,3 +19,26 @@ def flag_normal_outliers(series, n_sd):
     outliers = (series < m - n_sd * sd) | (series > m + n_sd * sd)
 
     return outliers
+
+def correct_normal_outliers(series, n_sd):
+    """ Replaces flagged outliers with the values of
+        the boundaries determined by n_sd.
+
+        Note: this function assumes a normal distribution.
+
+        :param series:      Series to flag outliers with
+        :param n_sd:        Number of standard deviations
+                                accepted within the distribution.
+
+        :return: outlier corrected series
+    """
+
+    m, sd = series.agg(['mean', 'std'])
+
+    # Flag outliers
+    outliers = flag_normal_outliers(series, n_sd)
+    signs = np.where(series > m, 1, -1)
+
+    series[outliers] = m + sd * signs[outliers] * n_sd
+
+    return series    
--- src/features/test_outlier_correction.py
+++ src/features/test_outlier_correction.py
@@ -16,3 +16,21 @@ class TestFlagNormalOutliers(unittest.TestCase):
 
         self.assertTrue(outliers.loc[500])
         self.assertTrue(outliers.loc[42])
+
+class TestCorrectNormalOutliers(unittest.TestCase):
+
+    def setUp(self):
+        self.series = pd.Series(np.random.normal(0, 0.1, 1000))
+
+        self.series.loc[500] = -5
+        self.series.loc[42] = 5
+
+    def test_correction(self):
+        m, sd = self.series.agg(['mean', 'std'])
+
+        corrected = oc.correct_normal_outliers(self.series.copy(), 5)
+
+        self.assertEqual(m - 5 * sd, corrected.loc[500])
+        self.assertEqual(m + 5 * sd, corrected.loc[42])
+
+        self.assertEqual(len(corrected), len(self.series))
